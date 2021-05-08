ff7b9fae571ac3d39feaddb2c2f8cf7ec2fc32e0
MoodyDreamer <38349760+ProphetAlgorithms@users.noreply.github.com>
2021-05-08 07:19:16 +0200

    Generic changes and non-essential path removals
    Return to the base name of the package, fix link of the icon and removed dirs not belonging to the application

--- aea-manager-deb-to-rpm.sh
+++ aea-manager-deb-to-rpm.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-## Author: Gianfranco Semeraro
-## Email: prophet.algorithms@gmail.com
-
 ARCH=$(uname -m)
+RELEASE="1"
 ALIEN_BIN="alien"
 ALIEN_CMD="sudo $ALIEN_BIN -r -g -c -k"
 SUMMARY_TAG="Summary:"
-MOD_RPMDIR_TAG="%define _rpmdir .\/"
 RPMDIR_TAG="%define _rpmdir ..\/"
+MOD_RPMDIR_TAG="%define _rpmdir .\/"
+#RPM_FILENAME_TAG="%define _rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm"
+#MOD_RPM_FILENAME_TAG="%define _rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm"
+DESCRIPTION_TAG=" <project description>"
+MOD_DESCRIPTION_TAG="Autonomous Economic Agents (AEA) Manager to easily build your agents."
+DESKTOP_FILE_TAG="Icon=aea_manager"
+MOD_DESKTOP_FILE_TAG="Icon=\/usr\/share\/icons\/hicolor\/0x0\/apps\/aea_manager.png"
+declare -a dir_tags=("%dir\s\\\"\/\\\"" "%dir\\s\\\"\/usr\/\\\"" "%dir\\s\\\"\/usr\/share\/\\\"" "%dir\\s\\\"\/usr\/share\/applications\/\\\"" "%dir\\s\\\"\/usr\/share\/icons\/\\\"" "%dir\\s\\\"\/usr\/share\/icons\/hicolor\/\\\"" "%dir\\s\\\"\/usr\/share\/icons\/hicolor\/0x0\/\\\"" "%dir\\s\\\"\/usr\/share\/icons\/hicolor\/0x0\/apps\/\\\"" "%dir\\s\\\"\/usr\/share\/doc\/\\\"" "%dir\\s\"\/opt\/\\\"")
 
 ## If the parameters are != 1 the execution ends:
 if [ "$#" -ne 1 ]; then
@@ -35,12 +40,12 @@ if [[ ${ALIEN_OUT} != "" ]] && [[ "${ALIEN_OUT[2]}" == "prepared." ]] && [ ${#AL
    exit 1
 fi
 
-## Extract the package folder end the package spec from the alien output:
+## Extract the package folder and the package spec from the alien output:
 ALIEN_DIR=${ALIEN_OUT[1]}
-ALIEN_SPEC="${ALIEN_DIR}/${ALIEN_DIR}-1.spec"
+ALIEN_SPEC="${ALIEN_DIR}/${ALIEN_DIR}-${RELEASE}.spec"
 
 ## Old code. Use grep to search for a string in a file, if found, it returns the line number:
-#SUMMARY_GREP_CMD="grep -n ${SUMMARY_TAG} ${ALIEN_DIR}/${ALIEN_DIR}-1.spec"
+#SUMMARY_GREP_CMD="grep -n ${SUMMARY_TAG} ${ALIEN_DIR}/${ALIEN_DIR}-${RELEASE}.spec"
 #SUMMARY_GREP_OUT=$(${SUMMARY_GREP_CMD})
 #SUMMARY_LINE=${SUMMARY_GREP_OUT%%:*}
 
@@ -53,6 +58,24 @@ bash -c "${SED_SUMMARY_CMD}"
 SED_RPMDIR_CMD="sudo sed -i 's/${RPMDIR_TAG}/${MOD_RPMDIR_TAG}/' ${ALIEN_SPEC}"
 bash -c "${SED_RPMDIR_CMD}"
 
+## Using sed to replace the line containing "%define _rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm"
+#SED_FILENAME_CMD="sudo sed -i 's/${RPM_FILENAME_TAG}/${MOD_RPM_FILENAME_TAG}/' ${ALIEN_SPEC}"
+#bash -c "${SED_FILENAME_CMD}"
+
+## Using sed to replace the line containing " <project description>"
+SED_DESCRIPTION_CMD="sudo sed -i 's/${DESCRIPTION_TAG}/${MOD_DESCRIPTION_TAG}/' ${ALIEN_SPEC}"
+bash -c "${SED_DESCRIPTION_CMD}"
+
+## Using sed to fix the path for application icon
+DESKTOP_FILE=${ALIEN_DIR}"/usr/share/applications/aea_manager.desktop"
+SED_DESKTOP_FILE_CMD="sudo sed -i 's/${DESKTOP_FILE_TAG}/${MOD_DESKTOP_FILE_TAG}/' ${DESKTOP_FILE}"
+bash -c "${SED_DESKTOP_FILE_CMD}"
+
+## Remove unnecessary dirs
+for dir in ${dir_tags[@]}; do
+  bash -c "sudo sed -i '/${dir}/d' ${ALIEN_SPEC}"
+done
+
 ## Starting rpmbuild to build the rpm package:
 CURR_DIR=`pwd`
 RPMBUILD_CMD="sudo rpmbuild --buildroot='${CURR_DIR}/${ALIEN_DIR}' -ba ${ALIEN_SPEC}"
@@ -60,15 +83,13 @@ echo "* Running command: ${RPMBUILD_CMD}"
 bash -c "${RPMBUILD_CMD}"
 
 ## Delete a file generated during package building:
-echo "* Delete file: /root/rpmbuild/SRPMS/${ALIEN_DIR}-1.src.rpm"
-sudo rm -rf /root/rpmbuild/SRPMS/${ALIEN_DIR}-1.src.rpm
+echo "* Delete file: /root/rpmbuild/SRPMS/${ALIEN_DIR}-${RELEASE}.src.rpm"
+sudo rm /root/rpmbuild/SRPMS/${ALIEN_DIR}-${RELEASE}.src.rpm
 
-## Rename and change the owner of the rpm package:
+## Change the owner of the rpm package:
 SUDO_USER=$(sudo printenv SUDO_USER)
-echo "* Remove \"-1\" from the rpm package version"
-sudo mv ${CURR_DIR}/${ALIEN_DIR}-1.${ARCH}.rpm ${CURR_DIR}/${ALIEN_DIR}_${ARCH}.rpm
-echo "* Change package owner (${SUDO_USER}): ${CURR_DIR}/${ALIEN_DIR}_${ARCH}.rpm"
-sudo chown ${SUDO_USER}:${SUDO_USER} ${CURR_DIR}/${ALIEN_DIR}_${ARCH}.rpm
+echo "* Change package owner (${SUDO_USER}): ${CURR_DIR}/${ALIEN_DIR}-${RELEASE}.${ARCH}.rpm"
+sudo chown ${SUDO_USER}:${SUDO_USER} ${CURR_DIR}/${ALIEN_DIR}-${RELEASE}.${ARCH}.rpm
 
 ## Python version warning, distributions like fedora have too recent versions of the software
 ## making aea-manager unusable:
